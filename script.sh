ed01111038c1603507fe6063bfa470b33d6fef96
Luiz Carlos Nascimento Junior <thecalifornia16@hotmail.com>
2024-07-29 16:46:53 -0300

    Update script.sh
    Adicionado instalação do docker-compose

--- script.sh
+++ script.sh
@@ -8,3 +8,10 @@ sudo apt-get install curl git zip -y
 sudo apt-get install docker.io -y
 sudo systemctl enable --now docker
 sudo systemctl start docker
+
+# Instalando o docker-compose
+sudo mkdir -p ~/.docker/cli-plugins/
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+sudo chmod +x ~/.docker/cli-plugins/docker-compose
+cd ~/.docker/cli-plugins/
+mv docker-compose /usr/local/bin/
