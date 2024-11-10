b173e17738a3f83c641cd320a450b4c4cc5a494e
Raphaela Vidal <118291210+raphavidall@users.noreply.github.com>
2024-11-10 00:27:54 -0300

    alteração script

--- script.sh
+++ script.sh
@@ -7,3 +7,6 @@ sudo apt-get install curl git zip -y
 sudo apt-get install docker.io -y
 sudo systemctl enable --now docker
 sudo systemctl start docker
+
+cd /home
+git clone git@github.com:raphavidall/bt-dvp-terraform-docker.git
