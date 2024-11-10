428862e416ddc9405cc03637fc4b2154f0503f48
Raphaela Vidal <118291210+raphavidall@users.noreply.github.com>
2024-11-10 10:24:36 -0300

    teste

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
-
 sudo apt-get install python3 -y
 sudo apt-get install curl git zip -y
 sudo apt-get install docker.io -y
 sudo systemctl enable --now docker
-sudo systemctl start docker
 
-cd /home
+cd /home/ubuntu  # Ou outra pasta com permissões de escrita
 git clone https://github.com/raphavidall/bt-dvp-terraform-docker.git
